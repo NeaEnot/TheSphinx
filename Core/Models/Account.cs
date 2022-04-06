375020682eceb751948741bb5b42c756c44b06c6
NeaEnot <oleg.kozloss@yandex.ru>
2022-04-06 22:08:03 +0400

    Аккаунт

---
+++ Core/Models/Account.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace TheSphinx.Core.Models
+{
+    /// <include file='Docs.xml' path='docs/members[@name="Account"]/Account/*'/>
+    public class Account
+    {
+        /// <include file='Docs.xml' path='docs/members[@name="Account"]/Id/*'/>
+        public string Id { get; set; }
+        /// <include file='Docs.xml' path='docs/members[@name="Account"]/Source/*'/>
+        public string Source { get; set; }
+        /// <include file='Docs.xml' path='docs/members[@name="Account"]/Fields/*'/>
+        public Dictionary<string, Field> Fields { get; set; }
+
+        public Account()
+        {
+            Fields = new Dictionary<string, Field>();
+        }
+    }
+}
---
+++ Core/Models/Field.cs
@@ -0,0 +1,11 @@
+﻿namespace TheSphinx.Core.Models
+{
+    /// <include file='Docs.xml' path='docs/members[@name="Field"]/Field/*'/>
+    public class Field
+    {
+        /// <include file='Docs.xml' path='docs/members[@name="Field"]/Value/*'/>
+        public string Value { get; set; }
+        /// <include file='Docs.xml' path='docs/members[@name="Field"]/Encrypted/*'/>
+        public bool Encrypted { get; set; }
+    }
+}
