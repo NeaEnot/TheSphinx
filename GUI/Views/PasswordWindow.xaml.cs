8d2fbff8a1030c9aaa3d97909adc271d2b378f4f
NeaEnot <oleg.kozloss@yandex.ru>
2022-08-16 11:32:02 +0400

    Окно ввода пароля

---
+++ GUI/Views/PasswordWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace GUI.Views
+{
+    public partial class PasswordWindow : Window
+    {
+        internal string Result { get; private set; }
+
+        public PasswordWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Result = passwordBox.Password;
+            DialogResult = true;
+        }
+    }
+}
