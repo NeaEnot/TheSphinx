e7ba366a31ee3216ca3780283565874152a1bdf1
NeaEnot <oleg.kozloss@yandex.ru>
2022-09-15 20:22:48 +0400

    Класс, проверяющий, ввёл ли пользователь пароль от зашифрованных полей

---
+++ GUI/PasswordController.cs
@@ -0,0 +1,37 @@
+﻿using GUI.Views;
+using System;
+
+namespace GUI
+{
+    internal class PasswordController
+    {
+        private TimeSpan duration;
+        private DateTime lastPasswordEnter;
+        private string password = "";
+
+        internal PasswordController(TimeSpan duration)
+        {
+            this.duration = duration;
+        }
+
+        internal string GetPassword()
+        {
+            if (DateTime.Now - lastPasswordEnter > duration)
+            {
+                PasswordWindow window = new PasswordWindow();
+
+                if (window.ShowDialog() == true)
+                {
+                    password = window.Result;
+                    lastPasswordEnter = DateTime.Now;
+                }
+                else
+                {
+                    password = "";
+                }
+            }
+
+            return password;
+        }
+    }
+}
