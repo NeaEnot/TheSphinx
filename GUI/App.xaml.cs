0ecea27ae97a8459a189d3a52f273eedf6d325e2
NeaEnot <oleg.kozloss@yandex.ru>
2022-11-03 11:24:52 +0400

    Главное окно со списком аккаунтов

--- GUI/App.xaml.cs
+++ GUI/App.xaml.cs
@@ -1,17 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
+using TheSphinx.Core.Logic;
 
 namespace GUI
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
+        internal static AccountLogic AccountLogic { get; private set; } = new AccountLogic();
     }
 }
