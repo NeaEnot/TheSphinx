33e6c79464a75a25d2cf246fc3ccc67e2f5b7a58
NeaEnot <oleg.kozloss@yandex.ru>
2022-11-23 14:41:24 +0400

    Вью модели для аккаунта и полей

---
+++ GUI/ViewModels/AccountViewModel.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using TheSphinx.Core.Models;
+
+namespace GUI.ViewModels
+{
+    internal class AccountViewModel : INotifyPropertyChanged
+    {
+        private Account account;
+
+        public string Source
+        {
+            get => account.Source;
+            set
+            {
+                account.Source = value;
+                OnPropertyChanged("Source");
+            }
+        }
+
+        public List<FieldViewModel> Fields { get; set; }
+
+        internal AccountViewModel(Account account)
+        {
+            this.account = account;
+            Fields = new List<FieldViewModel>();
+
+            foreach (KeyValuePair<string, Field> field in account.Fields)
+                Fields.Add(new FieldViewModel(field.Value, field.Key));
+        }
+
+        internal Account Convert()
+        {
+            Account account = new Account
+            {
+                Id = this.account.Id,
+                Source = this.account.Source
+            };
+
+            foreach (FieldViewModel field in Fields)
+                account.Fields.Add(field.Key, field.Field);
+
+            return account;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
---
+++ GUI/ViewModels/FieldViewModel.cs
@@ -0,0 +1,55 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using TheSphinx.Core.Models;
+
+namespace GUI.ViewModels
+{
+    internal class FieldViewModel : INotifyPropertyChanged
+    {
+        public Field Field { get; private set; }
+
+        private string key;
+        public string Key
+        {
+            get => key;
+            set
+            {
+                key = value;
+                OnPropertyChanged("Key");
+            }
+        }
+
+        public string Value
+        {
+            get => Field.Value;
+            set
+            {
+                Field.Value = value;
+                OnPropertyChanged("Value");
+            }
+        }
+
+        public bool Encrypted
+        {
+            get => Field.Encrypted;
+            set
+            {
+                Field.Encrypted = value;
+                OnPropertyChanged("Encrypted");
+            }
+        }
+
+        internal FieldViewModel(Field field, string key)
+        {
+            Field = field;
+            this.key = key;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
