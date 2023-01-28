e09f972065239239c82efb9f0bf74680ad536aee
NeaEnot <oleg.kozloss@yandex.ru>
2023-01-28 21:54:47 +0400

    Окно редактирования пользователя

---
+++ GUI/Views/UserWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+using TheSphinx.Core.Models;
+using TheSphinx.GUI.ViewModels;
+
+namespace TheSphinx.GUI.Views
+{
+    public partial class UserWindow : Window
+    {
+        private UserViewModel model;
+
+        public UserWindow(User user)
+        {
+            InitializeComponent();
+
+            model = new UserViewModel(user, App.NetworkLogic.RequiredFields);
+            DataContext = model;
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            User user = model.Convert();
+            App.UserLogic.Set(user, App.PasswordController.GetPassword(PasswordController.PasswordType.fields));
+
+            DialogResult = true;
+        }
+    }
+}
