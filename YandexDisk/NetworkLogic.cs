ed28a1ea6864bdb07863b2b3b83a8d0c177c14b0
NeaEnot <oleg.kozloss@yandex.ru>
2023-01-28 21:12:17 +0400

    Доработать сетевую логику с учетом изменений в классе контекста

--- YandexDisk/NetworkLogic.cs
+++ YandexDisk/NetworkLogic.cs
@@ -1,8 +1,9 @@
 ﻿using System;
 using System.IO;
 using System.Threading;
-using TheSphinx.Core;
 using TheSphinx.Core.Interfaces;
+using TheSphinx.Core.Logic;
+using TheSphinx.Core.Models;
 using YandexDisk.Client;
 using YandexDisk.Client.Clients;
 using YandexDisk.Client.Http;
@@ -13,26 +14,40 @@ namespace YandexDisk
     public class NetworkLogic : INetworkLogic
     {
         private IDiskApi diskApi;
+        private UserLogic userLogic;
+
+        private Func<string> getPassword;
+
+        /// <include file='Docs.xml' path='docs/members[@name="NetworkLogic"]/Constructor/*'/>
+        public NetworkLogic(Func<string> getPassword)
+        {
+            userLogic = new UserLogic();
+            this.getPassword = getPassword;
+        }
 
         public void Connect(Func<string> getCode)
         {
+            User user = userLogic.Get(getPassword());
+
             if (diskApi == null)
-                diskApi = new DiskHttpApi(Context.User.Fields["token"].Value);
+                diskApi = new DiskHttpApi(user.Fields["token"].Value);
         }
 
         public void Download(string path)
         {
             try
             {
+                User user = userLogic.Get(getPassword());
+
                 FileInfo file = new FileInfo(path);
                 if (file.Exists)
                     file.Delete();
 
-                diskApi.Files.DownloadFileAsync(Context.User.Fields["path"].Value, path).Wait();
+                diskApi.Files.DownloadFileAsync(user.Fields["path"].Value, path).Wait();
             }
             catch (Exception ex)
             {
-                throw new Exception("Не удалось скачать данные: " + ex.Message);
+                throw new Exception("Failed to download data: " + ex.Message);
             }
         }
 
@@ -40,11 +55,12 @@ namespace YandexDisk
         {
             try
             {
-                diskApi.Files.UploadFileAsync(Context.User.Fields["path"].Value, true, path, CancellationToken.None).Wait();
+                User user = userLogic.Get(getPassword());
+                diskApi.Files.UploadFileAsync(user.Fields["path"].Value, true, path, CancellationToken.None).Wait();
             }
             catch (Exception ex)
             {
-                throw new Exception("Не удалось загрузить данные: " + ex.Message);
+                throw new Exception("Failed to upload data: " + ex.Message);
             }
         }
     }
