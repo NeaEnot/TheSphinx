bd3474efac7bd7adcf088867ebcfd7ee6e8167c1
NeaEnot <oleg.kozloss@yandex.ru>
2023-02-05 22:38:28 +0400

    ~

--- Core/Helpers/StringConverter.cs
+++ Core/Helpers/StringConverter.cs
@@ -5,13 +5,13 @@ namespace TheSphinx.Core.Helpers
 {
     internal static class StringConverter
     {
-        internal static string Base64Encode(string str)
+        private static string Base64Encode(string str)
         {
             byte[] bytes = Encoding.UTF8.GetBytes(str);
             return Convert.ToBase64String(bytes);
         }
 
-        internal static string Base64Decode(string str)
+        private static string Base64Decode(string str)
         {
             byte[] bytes = Convert.FromBase64String(str);
             return Encoding.UTF8.GetString(bytes);
